531c4e7991862ed843ec13cb7c8e32882a260bb5
cleythonsantos <cleythondossantos09@gmail.com>
2025-03-18 10:31:50 -0300

    teste

--- story.py
+++ story.py
@@ -6,4 +6,3 @@ lugar = input("Onde estava o que o personagem encontrou? ")
 historia = (f"{personagem} estava em {cenário} quando de repente encontrou uma {evento} {lugar}. Isso mudou tudo para ele!")
     
 print("\nA sua história é: ")
-print(historia)
