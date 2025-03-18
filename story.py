36050fc62a3bfc300a03cb1ace48d94a994f65ba
cleythonsantos <cleythondossantos09@gmail.com>
2025-03-18 10:29:04 -0300

    atividade historia-interativa

---
+++ story.py
@@ -0,0 +1,9 @@
+cenário = input("Onde a história acontece? ")
+personagem = input("Quem é o personagem principal? ")
+evento = input("O que o personagem encontrou? ")
+lugar = input("Onde estava o que o personagem encontrou? ")
+
+historia = (f"{personagem} estava em {cenário} quando de repente encontrou uma {evento} {lugar}. Isso mudou tudo para ele!")
+    
+print("\nA sua história é: ")
+print(historia)
